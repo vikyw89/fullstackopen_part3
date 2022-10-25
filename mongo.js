12bc4f237fc7b567b89efb3675a8e8eb88821cfe
vikyw89 <vikyw89@gmail.com>
2022-10-25 23:43:49 +0800

    3.12 done

--- mongo.js
+++ mongo.js
@@ -13,28 +13,40 @@ const number = process.argv[4]
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.lslwh0b.mongodb.net/phoneBook?retryWrites=true&w=majority`
 
+mongoose.connect(url)
+
 const personSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  number: String,
-  time: Date
+    id: Number,
+    name: String,
+    number: String,
+    time: Date
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-mongoose
-    .connect(url)
-    .then(() => {
-        Person.find({}).then(result => {
-            result.forEach(person => {
-              console.log(person)
+if ((!name) || (!number)) {
+    console.log('phonebook:')
+    Person
+        .find({})
+        .then(result => {
+            result.forEach(n => {
+                console.log(n.name, n.number)
             })
             mongoose.connection.close()
         })
+}
+
+const person = new Person({
+    name: name,
+    number: number,
+})
+
+if ((name) && (number)) {
+  person
+    .save()
+    .then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
     })
-    
-    .then(() => {
-        console.log(`Added ${name} number ${number} to phonebook`)
-        return mongoose.connection.close()
-    })
-    .catch((err) => console.log(err))
+}
+        
