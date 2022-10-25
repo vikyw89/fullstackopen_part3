be84f1e1bfe129d55c50acccb7628516e35010b3
vikyw89 <vikyw89@gmail.com>
2022-10-25 14:59:27 +0800

    3.8 done

--- index.js
+++ index.js
@@ -26,7 +26,15 @@ let persons = [
 ]
   
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan('tiny', {
+    skip: function (tokens, req, res) { return tokens.method === 'POST'}
+}))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+    skip: function (tokens, req, res) { return tokens.method !== 'POST'}
+}))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
