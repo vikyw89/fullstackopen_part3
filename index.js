6c96518b8d023b4904eb763bbb1c59035ac7d233
vikyw89 <vikyw89@gmail.com>
2022-10-27 16:18:45 +0800

    updated backend for mongoose validation

--- index.js
+++ index.js
@@ -54,8 +54,13 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 
 app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
     Person
-        .findByIdAndUpdate(request.params.id, request.body, { new: true })
+        .findByIdAndUpdate(
+            request.params.id,
+            { name, number },
+            { new: true, runValidators: true, context: 'query' }
+        )
         .then(person => {
             if (person) {
                 return response.json(person)
@@ -77,22 +82,18 @@ app.delete('/api/persons/:id', (request, response) => {
         })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    if ((!body.name) || (!body.number)) {
-        return response.status(400).json({
-            error: 'name or number is missing'
-        })
-    }
     const person = new Person({
         name: body.name,
         number: body.number
     })
     person
         .save()
-        .then(person => {
-            return response.json(person)
+        .then(savedPerson => {
+            return response.json(savedPerson)
         })
+        .catch (error => next(error))
 })
 
 
@@ -108,8 +109,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
     next(error)
 }
 
--- models/person.js
+++ models/person.js
@@ -12,7 +12,11 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
   number: String
 })
 
