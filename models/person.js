4a7e2037a63ad446ef2109d91af1447d6d987a41
vikyw89 <vikyw89@gmail.com>
2022-10-27 18:03:07 +0800

    3.20 done

--- models/person.js
+++ models/person.js
@@ -19,7 +19,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 3
+    minLength: 8,
+    validate :{
+      validator: function(v){
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   }
 })
 
